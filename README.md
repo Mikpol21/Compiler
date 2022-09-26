c25c28c0b092314a6f61716be602e3a41c644e0a
Mikolaj <mikolajpolinski21@gmail.com>
2022-09-26 21:53:14 +0200

    Small fix

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 #Compiler
 ##Creating my own language
-
-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
--- src/AST.hs
+++ src/AST.hs
@@ -13,16 +13,6 @@ data Expr = Variable Id
     | Typed Expr Type 
     deriving (Show, Eq)
 
-formatExpr :: Expr -> String
-formatExpr = formatExpr' 0
-    where 
-        tabs 0 = ""
-        tabs n = "\t" ++ tabs (n - 1)   
-        formatExpr' n (Let typed e1 e2) = tabs n "let\n" ++ tabs (n + 1) ++ show typed ++ " = " ++ show e1 ++ tabs n "in\n" ++ tabs (n + 1) ++ show e2 ++ "\n"
-
-
-data Printer = Printer Int String
-
 data TypedParam = TypedParam Id Type
     deriving (Eq)
 instance Show TypedParam where
